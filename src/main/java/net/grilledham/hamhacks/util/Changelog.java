d6bce8fb49e4643a9e59d3ca913cbae53dc89265
GrilledHam <badebaggis@gmail.com>
2021-12-27 17:22:22 -0500

    Update to version 1.1.4

--- src/main/java/net/grilledham/hamhacks/util/Changelog.java
+++ src/main/java/net/grilledham/hamhacks/util/Changelog.java
@@ -3,6 +3,15 @@ package net.grilledham.hamhacks.util;
 public class Changelog {
 	
 	private static final String CHANGELOG = """
+			Update 1.1.4:
+				- Added settings to HUD module:
+					: Held Item Scale
+					: Shield Height
+					: Fire Height
+					: Model Bobbing Only
+					: No Hurt Cam
+				- Bug fixes
+			
 			Update 1.1.3:
 				- Bug fixes
 			
