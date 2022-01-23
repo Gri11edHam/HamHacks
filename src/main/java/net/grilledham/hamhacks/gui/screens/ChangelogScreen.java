e747e6ee73bc9062f2d922e6cf8d6345c9ed5460
GrilledHam <badebaggis@gmail.com>
2022-01-23 17:09:14 -0500

    Added changlog screen

---
+++ src/main/java/net/grilledham/hamhacks/gui/screens/ChangelogScreen.java
@@ -0,0 +1,66 @@
+package net.grilledham.hamhacks.gui.screens;
+
+import net.grilledham.hamhacks.gui.parts.impl.ButtonPart;
+import net.grilledham.hamhacks.util.Changelog;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.TranslatableText;
+
+public class ChangelogScreen extends Screen {
+	
+	private final Screen last;
+	
+	private ButtonPart backButton;
+	
+	public ChangelogScreen(Screen last) {
+		super(new TranslatableText("menu.hamhacks.changelog"));
+		this.last = last;
+	}
+	
+	@Override
+	protected void init() {
+		super.init();
+		backButton = new ButtonPart("Back", width / 2 - 100, height - 32, 200, 20, this::onClose);
+	}
+	
+	@Override
+	public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+		matrices.push();
+		
+		super.render(matrices, mouseX, mouseY, delta);
+		renderBackground(matrices);
+		float clw = 0;
+		for(String s : Changelog.getLatest().split("\n")) {
+			clw = Math.max(clw, client.textRenderer.getWidth(s.replace("\t", "    ")));
+		}
+		int i = 0;
+		for(String s : Changelog.getLatest().split("\n")) {
+			client.textRenderer.drawWithShadow(matrices, s.replace("\t", "    "), width / 2f - clw / 2f, 12 + i * 12, 0xffffffff);
+			i++;
+		}
+		backButton.draw(matrices, mouseX, mouseY, delta);
+		
+		matrices.pop();
+	}
+	
+	@Override
+	public boolean mouseClicked(double mouseX, double mouseY, int button) {
+		if(backButton.click(mouseX, mouseY, button)) {
+			return true;
+		}
+		return super.mouseClicked(mouseX, mouseY, button);
+	}
+	
+	@Override
+	public boolean mouseReleased(double mouseX, double mouseY, int button) {
+		if(backButton.release(mouseX, mouseY, button)) {
+			return true;
+		}
+		return super.mouseReleased(mouseX, mouseY, button);
+	}
+	
+	@Override
+	public void onClose() {
+		client.setScreen(last);
+	}
+}
