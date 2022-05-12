bfad8af2ce7df6ef6ac9f5b5d09955d848b4c634
GrilledHam <badebaggis@gmail.com>
2022-05-12 14:40:10 -0400

    Fixed loading setting overwriting possible values

--- src/main/java/net/grilledham/hamhacks/util/setting/settings/SelectionSetting.java
+++ src/main/java/net/grilledham/hamhacks/util/setting/settings/SelectionSetting.java
@@ -1,9 +1,5 @@
 package net.grilledham.hamhacks.util.setting.settings;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 import net.grilledham.hamhacks.util.setting.Setting;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
@@ -14,26 +10,24 @@ import java.util.List;
 
 public class SelectionSetting extends Setting<Text> {
 	
+	private final List<String> values = new ArrayList<>();
+	
 	public SelectionSetting(Text name, Text val, Text... vals) {
 		super(name);
 		if(!Arrays.asList(vals).contains(val)) {
 			throw new IllegalArgumentException("Possibilities do not include the current value: " + Arrays.toString(vals) + " - " + val);
 		}
-		JsonObject obj = new JsonObject();
-		obj.addProperty("selected", ((TranslatableText)val).getKey());
-		JsonArray values = new JsonArray();
 		for(Text s : vals) {
 			values.add(((TranslatableText)s).getKey());
 		}
-		obj.add("values", values);
-		value.add(((TranslatableText)name).getKey(), obj);
+		value.addProperty(((TranslatableText)name).getKey(), ((TranslatableText)val).getKey());
 		def = val;
 	}
 	
 	@Override
 	protected void updateValue(Text value) {
-		if(this.value.get(((TranslatableText)name).getKey()).getAsJsonObject().get("values").getAsJsonArray().contains(new JsonPrimitive(((TranslatableText)value).getKey()))) {
-			this.value.get(((TranslatableText)name).getKey()).getAsJsonObject().addProperty("selected", ((TranslatableText)value).getKey());
+		if(this.values.contains(((TranslatableText)value).getKey())) {
+			this.value.addProperty(((TranslatableText)name).getKey(), ((TranslatableText)value).getKey());
 		} else {
 			throw new IllegalArgumentException("Possibilities do not include the value to be set");
 		}
@@ -41,13 +35,13 @@ public class SelectionSetting extends Setting<Text> {
 	
 	@Override
 	public Text getValue() {
-		return new TranslatableText(value.get(((TranslatableText)name).getKey()).getAsJsonObject().get("selected").getAsString());
+		return new TranslatableText(value.get(((TranslatableText)name).getKey()).getAsString());
 	}
 	
 	public List<Text> getPossibleValues() {
 		List<Text> l = new ArrayList<>();
-		for(JsonElement val : value.get(((TranslatableText)name).getKey()).getAsJsonObject().get("values").getAsJsonArray()) {
-			l.add(new TranslatableText(val.getAsString()));
+		for(String val : values) {
+			l.add(new TranslatableText(val));
 		}
 		return l;
 	}
