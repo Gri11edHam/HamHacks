0c89d4447ae95fb96094726f91e9f99f1e9a4e4a
GrilledHam <badebaggis@gmail.com>
2022-06-09 18:33:29 -0400

    Update changelog

--- src/main/java/net/grilledham/hamhacks/util/Changelog.java
+++ src/main/java/net/grilledham/hamhacks/util/Changelog.java
@@ -3,6 +3,10 @@ package net.grilledham.hamhacks.util;
 public class Changelog {
 	
 	private static final String CHANGELOG = """
+			Update 1.5.1:
+				- Updated to 1.19
+				- Bug fixes
+				
 			Update 1.5:
 				- Added translations to module settings
 				- Added config versions
@@ -22,7 +26,7 @@ public class Changelog {
 					: Jetpack
 				- Added descriptions and tooltips
 				- Bug fixes
-			
+				
 			Update 1.4.2:
 				- Added the ability for modules to force-disable other modules
 				- Removed unnecessary settings in Fly
@@ -33,7 +37,7 @@ public class Changelog {
 				- Added Full Changelog screen
 				- Fixed translations not being used and updated translations
 				- Bug fixes
-			
+				
 			Update 1.4.1:
 				- Updated to 1.18.2
 				- Improved vanilla fly bypass
@@ -48,10 +52,10 @@ public class Changelog {
 				- Overhauled settings
 				- Added changelog button to title screen
 				- Bug fixes
-			
+				
 			Update 1.3.3:
 				- Fixed incompatibility with iris shaders
-			
+				
 			Update 1.3.2:
 				- Updated to 1.18.1
 				- Added settings to HUD module:
@@ -61,20 +65,20 @@ public class Changelog {
 					: Model Bobbing Only
 					: No Hurt Cam
 				- Bug fixes
-			
+				
 			Update 1.3.1:
 				- Fixed incompatibility with multiconnect
 				- Fixed color settings not saving chroma
-			
+				
 			Update 1.3:
 				- Updated to 1.18
-			
+				
 			Update 1.2:
 				- Bug fixes
-			
+				
 			Update 1.1.3:
 				- Bug fixes
-			
+				
 			Update 1.1.2:
 				- Added modules:
 					: Sprint
@@ -83,13 +87,13 @@ public class Changelog {
 					: Speed
 					: Fly
 				- Bug fixes
-			
+				
 			Update 1.1.1:
 				- Added modules:
 					: Click GUI
 					: HUD
 				- Added a new way to change keybinds and settings (Click GUI)
-			
+				
 			Update 1.1:
 				- Switched handling of keybinds from Minecraft's built in keybinds
 					: There is currently no way to change the keys besides editing the config
@@ -98,7 +102,7 @@ public class Changelog {
 				- Updated dependencies
 				- Renamed "Smooth Aim" to "Aimbot"
 				- Bug fixes
-			
+				
 			Update 1.0:
 				- Added modules:
 					: Fly
