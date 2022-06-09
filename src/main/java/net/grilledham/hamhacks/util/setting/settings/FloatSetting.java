55ebfff66ceeac80b82f4ae0fba983bec922aec2
GrilledHam <badebaggis@gmail.com>
2022-06-09 18:31:38 -0400

    Don't save/load constraints

--- src/main/java/net/grilledham/hamhacks/util/setting/settings/FloatSetting.java
+++ src/main/java/net/grilledham/hamhacks/util/setting/settings/FloatSetting.java
@@ -1,39 +1,39 @@
 package net.grilledham.hamhacks.util.setting.settings;
 
-import com.google.gson.JsonObject;
 import net.grilledham.hamhacks.util.setting.Setting;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableTextContent;
 
 public class FloatSetting extends Setting<Float> {
 	
+	private final float min;
+	private final float max;
+	
 	public FloatSetting(Text name, float val, float min, float max) {
 		super(name);
-		JsonObject obj = new JsonObject();
-		obj.addProperty("value", val);
-		obj.addProperty("min", min);
-		obj.addProperty("max", max);
-		value.add(((TranslatableTextContent)name.getContent()).getKey(), obj);
+		this.min = min;
+		this.max = max;
+		value.addProperty(((TranslatableTextContent)name.getContent()).getKey(), val);
 		def = val;
 	}
 	
 	@Override
 	protected void updateValue(Float value) {
-		if(value >= this.value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("min").getAsFloat() && value <= this.value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("max").getAsFloat()) {
-			this.value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().addProperty("value", value);
+		if(value >= min && value <= max) {
+			this.value.addProperty(((TranslatableTextContent)name.getContent()).getKey(), value);
 		}
 	}
 	
 	@Override
 	public Float getValue() {
-		return value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("value").getAsFloat();
+		return value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsFloat();
 	}
 	
 	public float getMin() {
-		return value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("min").getAsFloat();
+		return min;
 	}
 	
 	public float getMax() {
-		return value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("max").getAsFloat();
+		return max;
 	}
 }
--- src/main/java/net/grilledham/hamhacks/util/setting/settings/IntSetting.java
+++ src/main/java/net/grilledham/hamhacks/util/setting/settings/IntSetting.java
@@ -1,39 +1,39 @@
 package net.grilledham.hamhacks.util.setting.settings;
 
-import com.google.gson.JsonObject;
 import net.grilledham.hamhacks.util.setting.Setting;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableTextContent;
 
 public class IntSetting extends Setting<Integer> {
 	
+	private final int min;
+	private final int max;
+	
 	public IntSetting(Text name, int val, int min, int max) {
 		super(name);
-		JsonObject obj = new JsonObject();
-		obj.addProperty("value", val);
-		obj.addProperty("min", min);
-		obj.addProperty("max", max);
-		value.add(((TranslatableTextContent)name.getContent()).getKey(), obj);
+		this.min = min;
+		this.max = max;
+		value.addProperty(((TranslatableTextContent)name.getContent()).getKey(), val);
 		def = val;
 	}
 	
 	@Override
 	protected void updateValue(Integer value) {
-		if(value >= this.value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("min").getAsInt() && value <= this.value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("max").getAsInt()) {
-			this.value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().addProperty("value", value);
+		if(value >= min && value <= max) {
+			this.value.addProperty(((TranslatableTextContent)name.getContent()).getKey(), value);
 		}
 	}
 	
 	@Override
 	public Integer getValue() {
-		return value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("value").getAsInt();
+		return value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsInt();
 	}
 	
 	public int getMin() {
-		return value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("min").getAsInt();
+		return min;
 	}
 	
 	public int getMax() {
-		return value.get(((TranslatableTextContent)name.getContent()).getKey()).getAsJsonObject().get("max").getAsInt();
+		return max;
 	}
 }
