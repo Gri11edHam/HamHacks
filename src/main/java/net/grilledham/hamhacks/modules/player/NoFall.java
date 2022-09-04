1b357cf745ad7206aa3a177c699acaf10b94f42f
GrilledHam <badebaggis@gmail.com>
2022-09-04 19:43:36 -0400

    Switched Momentum mode with Fly Spoof

--- src/main/java/net/grilledham/hamhacks/modules/player/NoFall.java
+++ src/main/java/net/grilledham/hamhacks/modules/player/NoFall.java
@@ -6,19 +6,15 @@ import net.grilledham.hamhacks.modules.Keybind;
 import net.grilledham.hamhacks.modules.Module;
 import net.grilledham.hamhacks.util.SelectableList;
 import net.grilledham.hamhacks.util.setting.SelectionSetting;
-import net.minecraft.block.Material;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.network.packet.c2s.play.UpdatePlayerAbilitiesC2SPacket;
 import net.minecraft.text.Text;
-import net.minecraft.util.math.BlockPos;
 import org.lwjgl.glfw.GLFW;
 
 public class NoFall extends Module {
 	
 	@SelectionSetting(name = "hamhacks.module.noFall.mode")
-	public SelectableList mode = new SelectableList("hamhacks.module.noFall.mode.packet", "hamhacks.module.noFall.mode.packet", "hamhacks.module.noFall.mode.momentum");
-	
-	private float updates = 0;
-	private long lastTime;
+	public SelectableList mode = new SelectableList("hamhacks.module.noFall.mode.packet", "hamhacks.module.noFall.mode.packet", "hamhacks.module.noFall.mode.flySpoof");
 	
 	public NoFall() {
 		super(Text.translatable("hamhacks.module.noFall"), Category.PLAYER, new Keybind(GLFW.GLFW_KEY_N));
@@ -33,26 +29,19 @@ public class NoFall extends Module {
 						mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
 					}
 				}
-				case "hamhacks.module.noFall.mode.momentum" -> {
-					if(updates >= 0.5f) {
-						updates = 0;
-					}
-					
-					boolean isAboveBlock = false;
-					for(int xAdd = -1; xAdd < 2; xAdd++) {
-						for(int zAdd = -1; zAdd < 2; zAdd++) {
-							if(mc.world.getBlockState(new BlockPos(mc.player.getPos().subtract(0.3f * xAdd, 1, 0.3f * zAdd))).getMaterial() != Material.AIR) {
-								isAboveBlock = true;
-								break;
-							}
+				case "hamhacks.module.noFall.mode.flySpoof" -> {
+					if(mc.player.fallDistance >= 2) {
+						boolean wasFlying = mc.player.getAbilities().flying;
+						mc.player.getAbilities().flying = true;
+						mc.player.networkHandler.sendPacket(new UpdatePlayerAbilitiesC2SPacket(mc.player.getAbilities()));
+						mc.player.getAbilities().flying = wasFlying;
+						int yAdd = -2;
+						if(mc.player.fallDistance >= 3) {
+							yAdd = (int)mc.player.fallDistance;
 						}
+						mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getPos().x, mc.player.getPos().y + yAdd, mc.player.getPos().z, mc.player.isOnGround()));
+						mc.player.fallDistance /= 2;
 					}
-					if(!isAboveBlock) {
-						mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getPos().x, mc.player.getPos().y - updates, mc.player.getPos().z, mc.player.isOnGround()));
-					}
-					
-					updates += (System.currentTimeMillis() - lastTime) / 1000f;
-					lastTime = System.currentTimeMillis();
 				}
 			}
 		}
