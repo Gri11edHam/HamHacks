70ee6cbef656587ca42f7587789893bcc6b691f9
Gri11edHam <79022168+Gri11edHam@users.noreply.github.com>
2023-09-10 13:27:19 -0400

    fixed XRay when using the More Culling mod

---
+++ src/main/java/net/grilledham/hamhacks/mixin/moreculling/MixinCullingUtils.java
@@ -0,0 +1,25 @@
+package net.grilledham.hamhacks.mixin.moreculling;
+
+import ca.fxco.moreculling.utils.CullingUtils;
+import net.grilledham.hamhacks.modules.ModuleManager;
+import net.grilledham.hamhacks.modules.render.XRay;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CullingUtils.class)
+public class MixinCullingUtils {
+
+	@Inject(method = "shouldDrawSideCulling", at = @At("RETURN"), cancellable = true)
+	private static void shouldDrawSideCulling(BlockState thisState, BlockView world, BlockPos thisPos, Direction side, BlockPos sidePos, CallbackInfoReturnable<Boolean> cir) {
+		XRay xRay = ModuleManager.getModule(XRay.class);
+		if(xRay.isEnabled()) {
+			cir.setReturnValue(xRay.shouldDrawSide(thisState, world, thisPos, side, cir.getReturnValue()));
+		}
+	}
+}
