82249b5df2e2e8ad7fd08f62ae69e8c3127791a0
Gri11edHam <79022168+Gri11edHam@users.noreply.github.com>
2023-10-18 19:05:01 -0400

    improved performance

--- src/main/java/net/grilledham/hamhacks/modules/ModuleManager.java
+++ src/main/java/net/grilledham/hamhacks/modules/ModuleManager.java
@@ -1,56 +1,59 @@
 package net.grilledham.hamhacks.modules;
 
-import com.google.common.collect.Lists;
-
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ModuleManager {
 	
-	private static final List<Module> modules = Lists.newArrayList();
-	private static final Map<String, List<Module>> moduleMap = new HashMap<>();
+	private static final Map<Class<? extends Module>, Module> modules = new HashMap<>();
+	private static final Map<String, Map<Class<? extends Module>, Module>> moduleMap = new HashMap<>();
+	
+	private static List<Module> sortedModules = null;
 	
 	public static void register(String modId, Module m) {
-		modules.add(m);
+		modules.put(m.getClass(), m);
 		if(!moduleMap.containsKey(modId)) {
-			moduleMap.put(modId, new ArrayList<>());
+			moduleMap.put(modId, new HashMap<>());
 		}
-		moduleMap.get(modId).add(m);
+		moduleMap.get(modId).put(m.getClass(), m);
 	}
 	
 	public static void unregister(String modId, Module m) {
-		modules.remove(m);
-		moduleMap.get(modId).remove(m);
+		modules.remove(m.getClass());
+		moduleMap.get(modId).remove(m.getClass());
 	}
 	
 	public static List<Module> getModules() {
-		return modules;
+		if(sortedModules == null) sortModules(Comparator.comparing(Module::getName));
+		return sortedModules;
 	}
 	
 	public static List<Module> getModules(Category category) {
-		return modules.stream().filter(module -> module.category == category).toList();
+		if(sortedModules == null) sortModules(Comparator.comparing(Module::getName));
+		return sortedModules.stream().filter(module -> module.category == category).toList();
 	}
 	
 	public static List<Module> getModules(String modId) {
 		if(moduleMap.containsKey(modId)) {
-			return new ArrayList<>(moduleMap.get(modId));
+			return moduleMap.get(modId).values().stream().toList();
 		} else {
 			return new ArrayList<>();
 		}
 	}
 	
 	public static Module getModule(String name) {
-		return modules.stream().filter(module -> module.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+		return modules.values().stream().filter(module -> module.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
 	}
 	
 	public static synchronized <T extends Module> T getModule(Class<T> clazz) {
-		return clazz.cast(modules.stream().filter(module -> module.getClass() == clazz).findFirst().orElse(null));
+		return clazz.cast(modules.getOrDefault(clazz, null));
 	}
 	
 	public static void updateKeybinds() {
-		modules.forEach(Module::checkKeybind);
+		modules.forEach((c, m) -> m.checkKeybind());
 	}
 	
 	public static synchronized void sortModules(Comparator<Module> comparator) {
-		modules.sort(comparator);
+		sortedModules = modules.values().stream().sorted(comparator).collect(Collectors.toList());
 	}
 }
